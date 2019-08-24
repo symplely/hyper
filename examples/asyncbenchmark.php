6b9e7773dc7b33bc33eb3eefc4aef2c550a41bfb
techno-express <technoexpressnet@gmail.com>
2019-08-24 09:32:05 -0400

    an todo benchmark

---
+++ examples/asyncbenchmark.php
@@ -0,0 +1,24 @@
+<?php
+include 'vendor/autoload.php';
+
+function taskConcurrentRequests()
+{
+    $runs = 379;
+    $start = microtime(true);
+    for ($i = 0; $i < $runs; ++$i) {
+        $request[] = yield \request(\http_get('http://127.0.0.1:8081'));
+    }
+    $totalTime = microtime(true) - $start;
+    echo "Runs: " . number_format($runs) . "\n";
+    echo "Runs per second: " . floor($runs / $totalTime) . "\n";
+    echo "Average time per run: " . number_format(($totalTime / $runs) * 1000, 4) . " ms\n";
+    echo "Total time: " . number_format($totalTime, 4) . " s\n";
+
+    print_r($request);
+
+    \fetchOptions(2);
+    $responses = yield \fetch($request);
+    print_r($responses);
+}
+
+\coroutine_run(\taskConcurrentRequests());
